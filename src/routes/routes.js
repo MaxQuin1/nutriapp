9f9067b84a170bb3252b6804bd3a407e1ae2249e
NaiveCannonUT <riquicraft04@gmail.com>
2023-10-14 23:11:19 -0500

    pequeño avance de login y registro

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,18 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import Registro from '../pages/Registro'
 
 export const rutas = createBrowserRouter([
     {
         path: "/",
-        element: <Navbar/>
+        element: <Login/>
       },
       {
-        path: "/login",
-        element: <Login/>
+        path: "/Navbar",
+        element: <Navbar/>
       },
       {
         path: "/home",
         element: <Home/>
       },
+      {
+        path: "/Registro",
+        element: <Registro />
+      }
 ]);
