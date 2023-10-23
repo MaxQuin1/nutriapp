c2ac8df9ef2a488e4d17450ad11be022427fb37c
Goraz23 <angomezs.2303@gmail.com>
2023-10-23 10:38:59 -0500

    cambios entrega uno

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,15 +8,23 @@ import "../App.css";
 import "boxicons";
 
 export default function Navbar() {
-  //Se sube un cambio en forma de comentario Andrea
   return (
     <>
-      <div className="bg-[#ACDA7F] dark:bg-[#ACDA7F] dark:text-white  text-black fixed mb-0 top-0 left-0 w-full flex flex-row justify-between px-20 py-8 items-center">
-        <div>
-          <Link to={"/home"}>
-            <img src={"../img/logo.png"} className="h-4 w-4"/>
-          </Link>
-        </div>
+      <div className="bg-lime-200 w-screen h-[80px] z-10 drop-shadlow-lg">
+        <div className="px-2 flex justify-between items-center w-full h-full">
+          <div className="flex items-center">
+            <ul>
+              <li>
+                <Link to="../img/logo.png">
+                  <div className="w-4 h-full"></div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <ul className="hidden md:flex pr-4 font-bold">
+            <li>
+              <a href="/home">Home</a>
+            </li>
 
             <li>
               <a href="">Mis citas</a>
