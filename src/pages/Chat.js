52be2ad7a55c1edcfc75be7a3dc9916ffa5d8220
NaiveCannonUT <riquicraft04@gmail.com>
2023-10-31 08:18:28 -0500

    Se añadio pago, 2 nuevos componentes NavDash y se inicio chat e infoPaciente

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import NavDashChat from '../components/NavDashChat'
 
 export default function Chat() {
   return (
     <>
-    Chat
+    <Navbar></Navbar>
+    <NavDashChat></NavDashChat>
     </>
   )
 }
--- src/pages/InfoPaciente.js
+++ src/pages/InfoPaciente.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import NavDashInfo from '../components/NavDashInfo'
 
 export default function InfoPaciente() {
   return (
     <>
-    InfoPaciente
+    <Navbar></Navbar>
+    <NavDashInfo></NavDashInfo>
+    <h1  className="text-center text-3xl p-2">IMC</h1>
+    <button style={{ position: 'absolute', top: '115px', right: '100px' }} className="bg-lime-500 p-2 rounded-lg hover:bg-lime-400">Editar</button>
     </>
   )
 }
