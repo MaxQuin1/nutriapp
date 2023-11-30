54847e511cf14bc68b1a284f360348d7c5ae2efe
Goraz23 <angomezs.2303@gmail.com>
2023-11-30 03:42:55 -0500

    Se sube correción modal

--- src/components/AgregarAlimentos.js
+++ src/components/AgregarAlimentos.js
@@ -1,34 +1,28 @@
-import React from 'react'
-function AgregarAlimentos({ isOpen, onClose, onConfirm, comida, inputPlaceholder, inputValue, onInputChange }) {
-  const buttonColor = "#99CCFF";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+function AgregarAlimentos({ isOpen, onClose, onConfirm, comida}) {
+  const buttonColor = "#ae8efb";
 
   return (
     <>
       {isOpen && (
         <div className="fixed inset-0 shadow-2xl flex items-center justify-center z-50 rounded-xl">
           <div className="modal-overlay " onClick={onClose} />
-          <div className="modal-container bg-white w-96 mx-auto rounded-lg shadow-xl z-50 ">
-            <div className="modal-content p-4 bg-blue-200">
-              <p className="text-lg font-bold">{comida}</p>
-              <input
-                className='w-full border items-center rounded-lg p-1 m-auto'
-                type="text"
-                placeholder={inputPlaceholder}
-                value={inputValue}
-                onChange={onInputChange}
-              />
+          <div className="modal-container bg-white w-[50%] items-center mx-auto rounded-lg shadow-xl z-50 ">
+            <div className="modal-content p-4 bg-lime-400">
+              <p className="text-lg font-bold">Alimentos</p>
             </div>
             <div className="modal-actions p-4 bg-gray-100">
               <button
                 className="btn-primary mr-2 rounded-md p-2 "
-                style={{ backgroundColor: buttonColor, color: '#1e3a8a' }}
+                style={{ backgroundColor: buttonColor, color: 'black' }}
                 onClick={onConfirm}
               >
                 Confirmar
               </button>
               <button
                 className="btn-secondary rounded-md p-2"
-                style={{ backgroundColor: buttonColor, color: '#1e3a8a' }}
+                style={{ backgroundColor: buttonColor, color: 'black' }}
                 onClick={onClose}
               >
                 Cancelar
--- src/components/GraficaAlimentos.js
+++ src/components/GraficaAlimentos.js
@@ -1,8 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { Chart } from "react-google-charts";
+import AgregarAlimentos from "./AgregarAlimentos";
 
 export default function GraficaAlimentos() {
-  const tipo = localStorage.getItem('tipo_usuario')
+const tipo = localStorage.getItem('tipo_usuario')
+const [isModalOpen, setIsModalOpen] = useState(false);
+const [userToAdd, setUserToAdd] = useState("");
+const [users, setUsers] = useState([]);
+
+const handleOpenModal = () => {
+  console.log('Abriendo modal');
+  setIsModalOpen(true);
+};
+  
+const handleCloseModal = () => {
+  console.log('Cerrando modal');
+  setIsModalOpen(false);
+};
+
+const handleConfirmAction = () => {
+  if (userToAdd) {
+    setUsers([...users, userToAdd]);
+    setUserToAdd("");
+  }
+  handleCloseModal(); // Cerrar el modal después de confirmar
+};
+
+const handleUserInputChange = (e) => {
+  setUserToAdd(e.target.value);
+};
 
   const data2 = [
     ["Task", "Hours per Day"],
@@ -22,6 +48,7 @@ export default function GraficaAlimentos() {
   if(tipo === 'Nutricionista'){
     contenido = (
         <button
+        onClick={handleOpenModal}
         style={{ position: "absolute", top: "115px", right: "90px" }}
         className="bg-lime-300 p-2 rounded-lg hover:bg-lime-500"
       >
@@ -33,7 +60,16 @@ export default function GraficaAlimentos() {
   return (
     <div>
     <div className="bg-green-200 w-full ">
-    <button className="bg-green-500 text-2xl m-6 justify-center r-50 w-10"> Editar </button>
+
+    <AgregarAlimentos
+    isOpen={isModalOpen}
+    onClose={handleCloseModal}
+    onConfirm={handleConfirmAction}
+    message="Agrega tu comentario"
+    inputPlaceholder="Comentario"
+    inputValue={userToAdd}
+    onInputChange={handleUserInputChange}
+    />
     </div>
       {contenido}
       <div className="absolute right-[33%] border top-40">
@@ -48,108 +84,3 @@ export default function GraficaAlimentos() {
     </div>
   );
 }
-
-// import React, { useState } from "react";
-// import Navbar from "../components/Navbar";
-// import { Chart } from "react-google-charts";
-// import NavDashInfo from "../components/NavDashInfo";
-// import AgregarAlimentos from "../components/AgregarAlimentos";
-
-// export default function InfoPaciente() {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   const [userToAdd, setUserToAdd] = useState("");
-//   const [users, setUsers] = useState([]);
-
-//   const handleOpenModal = () => {
-//     console.log('Abriendo modal');
-//     setIsModalOpen(true);
-//   };
-  
-//   const handleCloseModal = () => {
-//     console.log('Cerrando modal');
-//     setIsModalOpen(false);
-//   };
-
-//   const handleConfirmAction = () => {
-//     if (userToAdd) {
-//       setUsers([...users, userToAdd]);
-//       setUserToAdd("");
-//     }
-//     handleCloseModal(); // Cerrar el modal después de confirmar
-//   };
-
-//   const handleUserInputChange = (e) => {
-//     setUserToAdd(e.target.value);
-//   };
-
-//   const data = [
-//     ["Meses", "asistidas", "faltas"],
-//     ["Enero", 400, 200],
-//     ["Febrero", 460, 250],
-//     ["Marzo", 1120, 300],
-//     ["Abril", 540, 350],
-//   ];
-
-//   const options = {
-//     chart: {
-//       title: "Registro de citas",
-//       subtitle: "",
-//     },
-//   };
-
-//   const data2 = [
-//     ["Task", "Hours per Day"],
-//     ["Huevo", 11],
-//     ["Brocoli", 2],
-//     ["Pollo sin grasas", 2],
-//     ["Manzana", 2],
-//     ["Ensalada", 7],
-//   ];
-
-//   const options2 = {
-//     title: "Alimentos más recomendados",
-//   };
-
-//   return (
-//     <>
-//       <Navbar></Navbar>
-//       <NavDashInfo></NavDashInfo>
-//       <h1 className="text-center text-3xl p-2">IMC</h1>
-//       <button
-//   onClick={handleOpenModal}
-//   className=""
-// >
-//   Editar
-// </button>
-//       <AgregarAlimentos
-//         isOpen={isModalOpen}
-//         onClose={handleCloseModal}
-//         onConfirm={handleConfirmAction}
-//         message="Agrega el alimento"
-//         inputPlaceholder="Alimento"
-//         inputValue={userToAdd}
-//         onInputChange={handleUserInputChange}
-//       />
-
-//       <div className="absolute right-[40%]">
-//         <Chart
-//           chartType="Bar"
-//           width="100%"
-//           height="400px"
-//           data={data}
-//           options={options}
-//         />
-//       </div>
-
-//       <div className="absolute right-[12%]">
-//         <Chart
-//           chartType="PieChart"
-//           data={data2}
-//           options={options2}
-//           width={"100%"}
-//           height={"400px"}
-//         />
-//       </div>
-//     </>
-//   );
-// }
